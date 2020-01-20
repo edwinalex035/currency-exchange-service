e7d622d5e5d0a7c1a3f3021de0db295002b228cf
Edwin Sanchez <edwinalex035@gmail.com>
2020-01-20 14:02:54 -0600

    Adding support for dynamic port

--- src/main/java/com/de/microservices/currencyexchangeservice/web/controller/CurrencyExchangeController.java
+++ src/main/java/com/de/microservices/currencyexchangeservice/web/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.de.microservices.currencyexchangeservice.web.controller;
 
 import com.de.microservices.currencyexchangeservice.domain.ExchangeValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,12 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue() {
-        return new ExchangeValue(100L, "USD", "INR", new BigDecimal(65));
+        return new ExchangeValue(100L, "USD", "INR", new BigDecimal(65), Integer.parseInt(environment.getProperty("local.server.port")));
     }
 
 }
